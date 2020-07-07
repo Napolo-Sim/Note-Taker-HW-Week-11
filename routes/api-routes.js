c2d183a02f308ab4705a3d1d4d55e35ffed3183d
Napoleon Vuong <napolo101@gmail.com>
2020-07-07 15:16:00 -0700

    Set up server.js to link with routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,3 +2,10 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
+router.get("api/notes", (req, res) => {
+    let data = fs.readFileSync("db.json", "utf-8");
+    data = JSON.parse(data);
+    res.json(data.title, data.text)
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3005;
+
+app.use(express.json());
+app.use(express.urlencoded({ extnded: true }));
+app.use(express.static("./public/"));
+
+const apiRoutes = require("./routes/api-routes");
+app.use(apiRoutes);
+
+const clientRoutes = require("./routes/client-routes");
+app.use(clientRoutes);
+
+app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
